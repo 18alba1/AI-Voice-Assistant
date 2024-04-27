89a97fbb031bf585f364d6f9c1c8081a522cac7f
Lellalu <lella.lu14@outlook.com>
2024-04-27 23:04:22 +0200

    update get current location tool

--- ecco6/tool/time.py
+++ ecco6/tool/time.py
@@ -2,9 +2,6 @@ import calendar
 import datetime
 
 
-from langchain.tools import StructuredTool
-
-
 def get_current_time() -> str:
     date = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     day_name = calendar.day_name[datetime.date.today().weekday()] 
